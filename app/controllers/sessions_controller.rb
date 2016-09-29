4d005df9a4e8bde1ec7d7375e9703da55f3b0d41
mokbeili <mo.kbeili@gmail.com>
2016-09-29 09:18:23 -0700

    switching branches

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email params[:email]
+    byebug
     session[:user_id] = user.id if user && user.authenticate(params[:password])
     redirect_to root_path
   end
